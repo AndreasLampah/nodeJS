25f5e9ca59a4ad9e569097e5f17b0de279ea7e82
AndreasLampah <andreaslampah97@gmail.com>
2024-06-07 20:35:15 +0800

    percobaan file html terpisah

--- belajar-req-res.js
+++ belajar-req-res.js
@@ -1,30 +1,32 @@
 
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
+
 
 let server = http.createServer(function (request, respon) {
     if (request.url == "/") {
         respon.writeHead(200, {"Content-Type" : "text/html"});
-        respon.write("halaman utama");
-        respon.end();
+        fs.createReadStream("./view/utama.html").pipe(respon);
+
     } else if (request.url == "/beranda") {
         respon.writeHead(200, {"Content-Type" : "text/html"});
-        respon.write("halaman beranda");
-        respon.end();
-    } else if (request.url == "/beranda") {
+        fs.createReadStream("./view/beranda.html").pipe(respon);
+       
+    } else if (request.url == "/profil") {
         respon.writeHead(200, {"Content-Type" : "text/html"});
         respon.write("profil saya");
         respon.end();
+
     } else {
         respon.writeHead(200, {"Content-Type" : "text/html"});
         respon.write("file tidak ada");
         respon.end();
     }
-
 })
 
 server.listen(port, () => {
-    console.log(`server sudah on, buka http://localHost:${port}`);
+    console.log(`server sudah on, buka http://localhost:${port}`);
 })
 
 
--- node-dasar.js
+++ node-dasar.js
@@ -1,15 +1,20 @@
+
+
 let nama = "andre";
 let alamat = "manado";
-
 
 
 const http = require("http");
 
 let server = http.createServer((req, res) => {
+    // mendefinisikan status code dan tipe konten yang ingin diberikan ke user
     res.writeHead(200, { "Content-Type" : "text/plain"});
+    // isi dari konten
     res.end(nama + alamat);
 })
-
+// konfigurasi port yang ingin digunakan
+// karena port default (80) sudah dipakai software lain
+// jadi kita pakai port yg free
 server.listen(3000, () => {
     console.log("server sudah siap<hr><hr>");
 })
